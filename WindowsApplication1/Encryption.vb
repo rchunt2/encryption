784b2b4087eb138f27d7ab28878295d228797fe4
rchunt2@gmail.com <rchunt2@gmail.com>
2017-10-10 07:25:13 -0400

    Add project files.

---
+++ WindowsApplication1/Encryption.vb
@@ -0,0 +1,28 @@
+﻿Imports System.Security.Cryptography
+Imports System.Text
+
+Public Class Encryption
+
+    Dim testVariable As String
+
+    Public Shared Function Encrypt(input As String) As String
+
+        Using hasher As MD5 = MD5.Create()    ' create hash object
+
+            ' Convert to byte array and get hash
+            Dim dbytes As Byte() =
+                 hasher.ComputeHash(Encoding.UTF8.GetBytes(input))
+
+            ' sb to create string from bytes
+            Dim sBuilder As New StringBuilder()
+
+            ' convert byte data to hex string
+            For n As Integer = 0 To dbytes.Length - 1
+                sBuilder.Append(dbytes(n).ToString("X2"))
+            Next n
+
+            Return sBuilder.ToString()
+        End Using
+
+    End Function
+End Class
---
+++ WindowsApplication1/Form1.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Security.Cryptography
+Imports System.Text
+
+Public Class Form1
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Encryption.Encrypt(TextBox1.Text).ToLower() = "1fe0802764aaba20e57a3999e6fe885e" Then
+            MsgBox("Success")
+        Else
+            MsgBox("Incorrect")
+        End If
+    End Sub
+
+
+
+End Class
